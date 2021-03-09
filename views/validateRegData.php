d07e38abb5f39c07c98c97d1442f3bc75565dfb3
Gurkenblut <vogel-lukas909@hotmail.com>
2021-03-09 16:21:30 +0100

    Update validateRegData.php

--- views/validateRegData.php
+++ views/validateRegData.php
@@ -1,26 +1,53 @@
 <?php
-header('Location: home');
-exit;
 
-if(isset($_SESSION['user'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $name = $_POST['name'];
-    $lastname = $_POST['lastname'];
-    $address = $_POST['address'];
-    $plz = $_POST['plz'];
-    $telephone = $_POST['telephone'];
-    $email = $_POST['email'];
+
+    echo "hey";
+
+    if (!empty($_POST)) {
+
+        $data = $_POST;
+        $isValid = true;
     
-    $statement = $pdo->prepare("SELECT * FROM user WHERE user_username == username");
-    $result = $statement->execute(array('username' => $username));
-    $user = $statement->fetch();
-}
+
+
+    
+
+        if (strlen($data['name']) < 3){
+            echo "Der Vorname ist zu Kurz";
+            $isValid = false;
+        }
+
+        if (strlen($data['lastname']) < 3){
+            echo "Der Nachname ist zu Kurz";
+            $isValid = false;
+        }
+
+        // Email überprüfen --> nicht leer, gültige emailadresse
+        if (empty($data['email']) || !preg_match('/^(\w{2,}|\w{2,}[\.\-]\w{2,})@(\w{2,}\.\w{2,}|\w{2,}[\.\-]\w{2,}\.\w{2,})$/', $data['email'])) {
+            echo 'Email entspricht nicht den Vorgaben';
+            $isValid = false;
+        }
+
+        
+
+
+        // $username = $_POST['username'];
+        // $password = $_POST['password'];
+        // $name = $_POST['name'];
+        // $lastname = $_POST['lastname'];
+        // $address = $_POST['address'];
+        // $plz = $_POST['plz'];
+        // $telephone = $_POST['telephone'];
+        // $email = $_POST['email'];
+        
+        // $statement = $pdo->prepare("SELECT * FROM user WHERE user_username == username");
+        // $result = $statement->execute(array('username' => $username));
+        // $user = $statement->fetch();
+    }
 
     //Überprüfung der Daten
     //Speichern der Session???
     //Evtl. Daten von erfolgreich.php übernehmen
 
-    header('Location: home');
 
 ?>
