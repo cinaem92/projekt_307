e15fc861399e9afb7d68977169cd3e1e2ffb319b
Lukas Vogel <vogell@ict-campus.net>
2021-03-10 13:27:29 +0100

    updatet validate registers

--- views/validateRegData.php
+++ views/validateRegData.php
@@ -56,6 +56,25 @@
         if($isValid){
             $_SESSION['data'] = $data;
             //daten in die Datenbank abspitzen
+            $db = new mysqli("localhost:3306", "test", "Welcome$21", "projekt_307");
+
+
+            $userQuery = $db->prepare("INSERT INTO user ('user_name', 'user_lastname', 'user_username', 'user_password', 'user_address', 'user_email', 'user_telephone')
+            VALUES (?,?,?,?,?,?,?)");
+
+            $userQuery->bind_param("sssssss",
+            $_SESSION['data']['name'], $_SESSION['data']['lastname'], $_SESSION['data']['username'], 
+            $_SESSION['data']['password'], $_SESSION['data']['address'], $_SESSION['data']['email'], 
+            $_SESSION['data']['telephone']);
+            
+            if ($userQuery->execute()){
+                header('neuerhund');
+                exit;
+            }
+
+            
+
+
         }
         
     } else {
